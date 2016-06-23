2bb6dc93a6a7f794929dd509560962a4815fd0e8
jingshuihuang <jingshuihuang@jingshuihuangdeimac.local>
2016-06-23 11:45:37 +0800

    Initial Commit

---
+++ QSXmppIM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QSXmppIM
+//
+//  Created by jingshuihuang on 16/6/23.
+//  Copyright © 2016年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
